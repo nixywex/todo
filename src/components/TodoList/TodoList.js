8528558b4985318d1773db08197fbf6af2110fc5
nick <nikitashel7@gmail.com>
2022-05-02 13:36:48 +0300

    change some todo logic

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,126 +1,95 @@
-import React from "react";
+import React, { useContext } from "react";
 import Task from "../Task/Task";
-import TodoCore from "../todo_components/TodoCore";
-import Popup from "../Popup/Popup";
+import { Context } from "../../context";
 
 import styles from "./TodoList.module.scss";
-import Button from "../UI/Button/Button";
-import Input from "../UI/Input/Input";
 
-function TodoList({ controller }) {
-	const core = TodoCore();
-	const activeFolder = controller.activeFolder;
-	const [isPopupActive, setIsPopupActive] = React.useState(false);
-	const [popupTaskID, setPopupTaskID] = React.useState(null);
+function TodoList({ activeFolder, tasks }) {
+	const {
+		addTask,
+		changeIsDone,
+		deleteTask,
+		changeIsImportant,
+		changeFolder,
+		addFolder,
+		sortTasks,
+	} = useContext(Context);
 
-	const handleDeleteClick = (id) => {
-		core.deleteTask(id);
-	};
+	// const [isPopupActive, setIsPopupActive] = React.useState(false);
+	// const [popupTaskID, setPopupTaskID] = React.useState(null);
 
-	const handleImportantClick = (id) => {
-		core.changeIsImportant(id);
-	};
+	// const handlePopupClick = (id = null) => {
+	// 	setIsPopupActive(!isPopupActive);
+	// 	setPopupTaskID(id);
+	// };
 
-	const handleDoneClick = (id) => {
-		core.changeIsDone(id);
-	};
+	// const handleTaskChangeClick = (id, defaultValue) => {
+	// 	const newTask = prompt("Введите задачу", defaultValue);
+	// 	core.changeTask(id, newTask);
+	// };
 
-	const handleFolderClick = (id, defaultValue) => {
-		const newFolder = prompt("Введите название папки: ", defaultValue);
-		core.changeFolder(newFolder, id);
-		controller.addFolder(newFolder);
-	};
+	// const handleDiscriptionChange = (id, text) => {
+	// 	core.changeDiscription(id, text);
+	// };
 
-	const handleAddTaskClick = (defaultValue) => {
-		core.addTask(prompt("Введите задачу"), activeFolder);
-	};
+	// const generatePopup = () => {
+	// 	let task = core
+	// 		.getTasks()
+	// 		.filter((activeTask) => activeTask.id == popupTaskID)[0];
+	// 	console.log(task.discription);
+	// 	console.log(task);
 
-	const handlePopupClick = (id = null) => {
-		setIsPopupActive(!isPopupActive);
-		setPopupTaskID(id);
-	};
+	// 	return (
+	// 		<Popup trigger={isPopupActive}>
+	// 			<div className={styles.popupBody}>
+	// 				<div className={styles.header}>
+	// 					<h1
+	// 						onClick={() => {
+	// 							handleTaskChangeClick(task.id, task.task);
+	// 							setIsPopupActive(false);
+	// 						}}
+	// 						className={styles.popupTask}
+	// 					>
+	// 						{task.task}
+	// 					</h1>
+	// 					<div className={styles.info}>
+	// 						<p className={styles.popupIsImportant}>
+	// 							{task.isImportant ? "⭐️" : ""}
+	// 						</p>
+	// 						<p className={styles.popupIsDone}>
+	// 							{task.isComplete ? "✅" : ""}
+	// 						</p>
+	// 					</div>
+	// 				</div>
+	// 				<h3
+	// 					onClick={() => handleFolderClick(task.id, task.folder)}
+	// 					className={styles.popupFolder}
+	// 				>
+	// 					{task.folder}
+	// 				</h3>
+	// 				<textarea
+	// 					onChange={(event) => {
+	// 						handleDiscriptionChange(task.id, event.target.value);
+	// 					}}
+	// 					className={styles.popupDiscription}
+	// 					cols='30'
+	// 					rows='10'
+	// 					defaultValue={task.discription}
+	// 					placeholder={"Описание"}
+	// 				></textarea>
 
-	const handleTaskChangeClick = (id, defaultValue) => {
-		const newTask = prompt("Введите задачу", defaultValue);
-		core.changeTask(id, newTask);
-	};
-
-	const handleDiscriptionChange = (id, text) => {
-		core.changeDiscription(id, text);
-	};
-
-	const generateTask = (item) => {
-		return (
-			<Task
-				handlePopupClick={handlePopupClick}
-				changeFolder={handleFolderClick}
-				changeIsDone={handleDoneClick}
-				changeIsImportant={handleImportantClick}
-				deleteClick={handleDeleteClick}
-				task={item}
-				key={item.id}
-			/>
-		);
-	};
-
-	const generatePopup = () => {
-		let task = core
-			.getTasks()
-			.filter((activeTask) => activeTask.id == popupTaskID)[0];
-		console.log(task.discription);
-		console.log(task);
-
-		return (
-			<Popup trigger={isPopupActive}>
-				<div className={styles.popupBody}>
-					<div className={styles.header}>
-						<h1
-							onClick={() => {
-								handleTaskChangeClick(task.id, task.task);
-								setIsPopupActive(false);
-							}}
-							className={styles.popupTask}
-						>
-							{task.task}
-						</h1>
-						<div className={styles.info}>
-							<p className={styles.popupIsImportant}>
-								{task.isImportant ? "⭐️" : ""}
-							</p>
-							<p className={styles.popupIsDone}>
-								{task.isComplete ? "✅" : ""}
-							</p>
-						</div>
-					</div>
-					<h3
-						onClick={() => handleFolderClick(task.id, task.folder)}
-						className={styles.popupFolder}
-					>
-						{task.folder}
-					</h3>
-					<textarea
-						onChange={(event) => {
-							handleDiscriptionChange(task.id, event.target.value);
-						}}
-						className={styles.popupDiscription}
-						cols='30'
-						rows='10'
-						defaultValue={task.discription}
-						placeholder={"Описание"}
-					></textarea>
-
-					<div className={styles.buttons}>
-						<button
-							onClick={() => handlePopupClick()}
-							className={styles.popupCloseButton}
-						>
-							Закрыть
-						</button>
-					</div>
-				</div>
-			</Popup>
-		);
-	};
+	// 				<div className={styles.buttons}>
+	// 					<button
+	// 						onClick={() => handlePopupClick()}
+	// 						className={styles.popupCloseButton}
+	// 					>
+	// 						Закрыть
+	// 					</button>
+	// 				</div>
+	// 			</div>
+	// 		</Popup>
+	// 	);
+	// };
 
 	// const generateAddTaskPopup = () => {
 	// 	return (
@@ -138,27 +107,51 @@ function TodoList({ controller }) {
 	// 		</div>
 	// 	);
 	// };
-	core.sort();
+	// core.sort();
+
+	const handleAddTaskClick = (defaultValue) => {
+		addTask(prompt("Введите задачу"), activeFolder);
+	};
+
+	const generateTask = (item) => {
+		return (
+			<Task
+				// handlePopupClick={handlePopupClick}
+				changeFolder={() => {
+					const newFolder = prompt("Введите новую папку:", item.folder);
+					changeFolder(newFolder, item.id);
+					addFolder(newFolder);
+				}}
+				changeIsDone={() => changeIsDone(item.id)}
+				changeIsImportant={() => changeIsImportant(item.id)}
+				deleteClick={() => deleteTask(item.id)}
+				task={item}
+				key={item.id}
+			/>
+		);
+	};
+
+	sortTasks();
 
 	return (
 		<div className={styles.wrapper}>
 			{/* {generateAddTaskPopup()} */}
-			{isPopupActive && popupTaskID ? generatePopup() : ""}
-			{core.getTasks().map((item) => {
+			{/* {isPopupActive && popupTaskID ? generatePopup() : ""} */}
+			{tasks.map((item) => {
 				if (activeFolder == "Активные") {
 					if (!item.isComplete) {
-						return generateTask(item, handlePopupClick);
+						return generateTask(item);
 					}
 				} else if (activeFolder == "Выполненные") {
 					if (item.isComplete) {
-						return generateTask(item, handlePopupClick);
+						return generateTask(item);
 					}
 				} else {
 					if (
 						item.folder.toLowerCase() == activeFolder.toLowerCase() &&
 						!item.isComplete
 					) {
-						return generateTask(item, handlePopupClick);
+						return generateTask(item);
 					}
 				}
 			})}
