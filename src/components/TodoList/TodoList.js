67a1eeefcfed99c66a4e54fcaa621958396c7356
nick <nikitashel7@gmail.com>
2022-05-07 20:39:00 +0300

    add counter of tasks and delete button for all of tasks

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -6,29 +6,44 @@ import Task from "../Task/Task";
 import styles from "./TodoList.module.scss";
 
 function TodoList({ tasks }) {
-	const { handleAddTaskClick, openPopup } =
-		useContext(Context);
+  const { handleAddTaskClick, openPopup, deleteTask } = useContext(Context);
 
-	return (
-		<div className={styles.wrapper}>
-			{tasks.map((item) => {
-				return <Task task={item} key={item.id} />;
-			})}
-			<button
-				className={styles.addButton}
-				onClick={() =>
-					openPopup("input", {
-						text: "Добавить задачу",
-						placeholder: "Введите задачу",
-						trueButton: handleAddTaskClick,
-						args: [],
-					})
-				}
-			>
-				+
-			</button>
-		</div>
-	);
+  return (
+    <div className={styles.wrapper}>
+      {tasks.map((item) => {
+        return <Task task={item} key={item.id} />;
+      })}
+      {tasks.length !== 0 ? (
+        <div className={styles.messageDiv}>
+          <p className={styles.messageInfo}>Всего задач: {tasks.length}</p>
+          <p
+            className={styles.delete}
+            onClick={() =>
+              openPopup("bool", {
+                text: "Удалить задачу?",
+                trueButton: () => tasks.forEach((item) => deleteTask(item.id)),
+              })
+            }
+          >
+            Удалить все в папке
+          </p>
+        </div>
+      ) : null}
+      <button
+        className={styles.addButton}
+        onClick={() =>
+          openPopup("input", {
+            text: "Добавить задачу",
+            placeholder: "Введите задачу",
+            trueButton: handleAddTaskClick,
+            args: [],
+          })
+        }
+      >
+        +
+      </button>
+    </div>
+  );
 }
 
 export default TodoList;
